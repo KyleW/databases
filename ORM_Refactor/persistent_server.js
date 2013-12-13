3ff726fc38542515d509be1387ca1f0e8001b411
Hack Reactor Students <kyle.warneck@gmail.com>
2013-12-12 20:00:28 -0800

    Implemented ORM for gets

--- ORM_Refactor/persistent_server.js
+++ ORM_Refactor/persistent_server.js
@@ -5,67 +5,82 @@ var fs = require('fs');
 var Sequelize = require("sequelize");
 
 
+var sequelize = new Sequelize("chatterbox", "root", null, {
+  dialect: 'mysql',
+  port: 3306
+});
 
-var sequelize = new Sequelize("chat", "bacon", "pass");
-/* TODO this constructor takes the database name, username, then password.
- * Modify the arguments if you need to */
+sequelize
+  .authenticate()
+  .complete(function(err) {
+    if (!!err) {
+      console.log('Unable to connect to the database.');
+    } else {
+      console.log('Connection has been established successfully.');
+    }
+  });
+
 
 /* first define the data structure by giving property names and datatypes
  * See http://sequelizejs.com for other datatypes you can use besides STRING. */
 var User = sequelize.define('User', {
-  user_name: Sequelize.STRING,
+  user_name: Sequelize.STRING
 });
 
-/* .sync() makes Sequelize create the database table for us if it doesn't
- *  exist already: */
-User.sync().success(function() {
-  /* This callback function is called once sync succeeds. */
+var Message = sequelize.define('Message',{
+  text: Sequelize.TEXT
+});
 
-  // now instantiate an object and save it:
-  var newUser = User.build({user_name: "Jean Valjean"});
-  newUser.save().success(function() {
+var Room = sequelize.define('Room',{
+  roomname: Sequelize.STRING
+});
 
-    /* This callback function is called once saving succeeds. */
+User.hasMany(Message);
+Message.belongsTo(User);
 
-    // Retrieve objects from the database:
-    User.findAll({ where: {user_name: "Jean Valjean"} }).success(function(usrs) {
-      // This function is called back with an array of matches.
-      for (var i = 0; i < usrs.length; i++) {
-        console.log(usrs[i].user_name + " exists");
-      }
-    });
+Room.hasMany(Message);
+Message.belongsTo(Room);
+
+
+sequelize
+  .sync({ force: true })
+  .complete(function(err) {
+     if (!!err) {
+       console.log('An error occurred while create the table.');
+     } else {
+       console.log('It worked!');
+
+      var newUsr = User.create({
+        user_name:"Jean Valjean"
+      });
+
+      var newRoom = Room.create({
+        roomname: "Some room"
+      });
+
+      var newMsg = Message.create({
+        user_name: "Jean Valjean",
+        text: "I am the very model of a modern major general.",
+        roomname: "Some room"
+      }).complete(function(err) {
+        if (!!err) {
+          console.log('The instance has not been saved.');
+        } else {
+          console.log('We have a persisted instance now');
+        }
+      });
+    }
   });
-});
 
-
-
-
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  database : 'chat'
-});
-
-// connection.end();
-connection.connect();
-
-// connection.query("INSERT into users (username) values ('testUser')", function(err, rows, fields) {
-//   if (err) throw err;
-
-//   // console.log('Inserted: ', rows[0]);
+//   // Retrieve objects from the database:
+// User.findAll({ where: {user_name: "Jean Valjean"} }).success(function(usrs) {
+//     // This function is called back with an array of matches.
+//     for (var i = 0; i < usrs.length; i++) {
+//       console.log(usrs[i].user_name + " exists");
+//     }
+//   });
 // });
 
-// connection.query("SELECT * from users", function(err, rows, fields) {
-//   if (err) throw err;
-
-//   console.log('Inserted: ', rows[0]);
-// });
-
-// connection.end();
-
-
-var holder={};
 
 
 
@@ -73,118 +88,128 @@ var sendResponse = function(request,response,sendMe,contentType,status) {
   status = status || 200;
   contentType = contentType || 'text/html';
   response.writeHeader(status,{'Content-Type': contentType});
-  // response.write();
   response.end(sendMe);
-  // connection.end();
 };
 
 
-////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////
 
-var getUserid = function (newMessage) {
-  connection.query("SELECT user_id from users WHERE username='" + newMessage.username + "'", function (err, rows, fields) {
-    if (err) throw err;
-    if (rows[0]){
-      newMessage.user_id = rows[0].user_id;
-      // console.log("existing user id is: ",user);
-      getRoomid(newMessage);
-    } else {
-      connection.query("INSERT into users (username) values ('" + newMessage.username + "')", function (err,result) {
-        if (err) throw err;
-        newMessage.user_id = result.insertId;
-        // console.log("new user id is: ",user);
-        getRoomid(newMessage);
-      });
-    }
-  });
-};
+// var getUserid = function (newMessage) {
+//   connection.query("SELECT user_id from users WHERE username='" + newMessage.username + "'", function (err, rows, fields) {
+//     if (err) throw err;
+//     if (rows[0]){
+//       newMessage.user_id = rows[0].user_id;
+//       // console.log("existing user id is: ",user);
+//       getRoomid(newMessage);
+//     } else {
+//       connection.query("INSERT into users (username) values ('" + newMessage.username + "')", function (err,result) {
+//         if (err) throw err;
+//         newMessage.user_id = result.insertId;
+//         // console.log("new user id is: ",user);
+//         getRoomid(newMessage);
+//       });
+//     }
+//   });
+// };
 
-var getRoomid = function (newMessage){
-  connection.query("SELECT room_id from rooms WHERE roomname='" + newMessage.roomname + "'", function (err, rows, fields) {
-    if (err) throw err;
-    if (rows[0]){
-      newMessage.room_id = rows[0].room_id;
-      insertMessage(newMessage);
-    } else {
-      connection.query("INSERT into rooms (roomname) values ('" + newMessage.roomname + "')", function (err, result) {
-        if (err) throw err;
-        newMessage.room_id = result.insertId;
-        insertMessage(newMessage);
-      });
-    }
-  });
-};
+// var getRoomid = function (newMessage){
+//   connection.query("SELECT room_id from rooms WHERE roomname='" + newMessage.roomname + "'", function (err, rows, fields) {
+//     if (err) throw err;
+//     if (rows[0]){
+//       newMessage.room_id = rows[0].room_id;
+//       insertMessage(newMessage);
+//     } else {
+//       connection.query("INSERT into rooms (roomname) values ('" + newMessage.roomname + "')", function (err, result) {
+//         if (err) throw err;
+//         newMessage.room_id = result.insertId;
+//         insertMessage(newMessage);
+//       });
+//     }
+//   });
+// };
 
-var insertMessage = function (newMessage) {
-  var query = "INSERT INTO messages (room_id, user_id, text) VALUES (";
-  query += newMessage.room_id + " , " + newMessage.user_id + " , ' " + newMessage.text + " ' )";
+// var insertMessage = function (newMessage) {
+//   var query = "INSERT INTO messages (room_id, user_id, text) VALUES (";
+//   query += newMessage.room_id + " , " + newMessage.user_id + " , ' " + newMessage.text + " ' )";
 
-  console.log ("Here's the query we're running in mysql: ",query);
+//   console.log ("Here's the query we're running in mysql: ",query);
 
-  connection.query(query, function(err, rows, fields) {
-    // "INSERT into messages (text, user_id, room_id) values ('" + newMessage.text + "')", function(err, rows, fields) {
-    // "INSERT into messages set ?""{user_id:}
-    if (err) throw err;
-    sendResponse(holder.request, holder.response, '', 'application/json', 201);
-    // connection.end();
-  });
-};
+//   connection.query(query, function(err, rows, fields) {
+//     // "INSERT into messages (text, user_id, room_id) values ('" + newMessage.text + "')", function(err, rows, fields) {
+//     // "INSERT into messages set ?""{user_id:}
+//     if (err) throw err;
+//     sendResponse(holder.request, holder.response, '', 'application/json', 201);
+//     // connection.end();
+//   });
+// };
 
 var handleRequest = function(request, response) {
 
-  holder.request=request;
-  holder.response=response;
+  // holder.request=request;
+  // holder.response=response;
 
   var toSend = "";
   var statusCode= 404;
   var urlObject = url.parse(request.url);
   var requestURL = urlObject.pathname.split('/');
 
-  /* Without this line, this server wouldn't work. See the note below about CORS. */
   var headers = defaultCorsHeaders;
 
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   if (requestURL[1] === 'classes') {
-    if (request.method === "GET"){
-      connection.query("SELECT * from messages", function(err, rows, fields) {
-        if (err) throw err;
-        sendResponse(request, response, JSON.stringify(rows), 'application/json');
-        console.log('Heres what we sent: ', rows);
-      });
-    }
+    if (request.method === "GET") {
+      Message
+        .findAll()
+        .complete(function(err, data) {
+          if (!!err) {
+            console.log('An error occurred while searching for John!');
+          // } else if (!johnDoe) {
+          //   console.log('No user with the username "john-doe" has been found.');
+          } else {
+            sendResponse(request, response, JSON.stringify(data), 'application/json');
+          }
+        });
 
-
-    if (request.method === "POST"){
-      statusCode = 201;
-      headers['Content-Type'] = 'application/json';
-      var fullbody = '';
-
-      request.on('data', function(chunk){
-        fullbody += chunk;
-      });
-
-      request.on('end', function(){
-        var newMessage = JSON.parse(fullbody);
-
-        // console.log("This is what we got from a post request ", fullbody);//debugging
-
-        //Get user_id or create user_id
-        getUserid(newMessage);
-
-      });
+      // connection.query("SELECT * from messages", function(err, rows, fields) {
+      //   if (err) throw err;
+      //   sendResponse(request, response, JSON.stringify(rows), 'application/json');
+      //   console.log('Heres what we sent: ', rows);
+      // });
     }
   }
 
 
-////////// SERVES STATIC FILES ////////////////////
+//     if (request.method === "POST"){
+//       statusCode = 201;
+//       headers['Content-Type'] = 'application/json';
+//       var fullbody = '';
 
-  else if (requestURL[1] === "" && request.method === "GET"){
-    fs.readFile('../chatClient/index.html', function(err, html) {
-      if (err) {throw err;}
-      sendResponse(request,response,html);
-    });
-  }
+//       request.on('data', function(chunk){
+//         fullbody += chunk;
+//       });
+
+//       request.on('end', function(){
+//         var newMessage = JSON.parse(fullbody);
+
+//         // console.log("This is what we got from a post request ", fullbody);//debugging
+
+//         //Get user_id or create user_id
+//         getUserid(newMessage);
+
+//       });
+//     }
+//   }
+
+
+// ////////// SERVES STATIC FILES ////////////////////
+
+else if (requestURL[1] === "" && request.method === "GET"){
+  fs.readFile('../chatClient/index.html', function(err, html) {
+    if (err) {throw err;}
+    sendResponse(request,response,html);
+  });
+}
 
   else if (requestURL[1] === "scripts" && requestURL[2] === "models" && request.method === "GET"){ //Fix This line
     var file = requestURL[3];
@@ -219,6 +244,8 @@ var handleRequest = function(request, response) {
   }
 };
 
+
+
 exports.handleRequest = handleRequest;
 
 var defaultCorsHeaders = {
@@ -227,3 +254,7 @@ var defaultCorsHeaders = {
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
 };
+
+
+
+
